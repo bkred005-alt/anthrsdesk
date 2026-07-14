6627f59f006630e2828870604ee4ca9f7e1b29c0
bkred005-alt <bkred005@gmail.com>
2026-07-14 21:35:53 +1000

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ document.getElementById('mail-icon').addEventListener('click', function() {
 function closeWindow(id) {
   document.getElementById(id).style.display = 'none';
 }
+const cdIcon = document.getElementById('cd-icon');
+const music = document.getElementById('bg-music');
+let isPlaying = false;
+
+cdIcon.addEventListener('click', function() {
+  if (isPlaying) {
+    music.pause();
+  } else {
+    music.play();
+  }
+  isPlaying = !isPlaying;
+});
